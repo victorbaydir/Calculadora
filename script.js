d8c27e5d0920affb4fc85e49634a553b26779ac6
victorbaydir <victorbaydir@hotmail.com>
2023-07-06 21:32:53 -0300

    alteracoes, add eventListener

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-const buttons = document.querySelectorAll('button')
-            console.log(buttons)
+const buttons = document.querySelectorAll('button');
+var result = document.getElementById("resultado")
+var var_op;
+
+buttons.forEach((button) => {
+    button.addEventListener("click",(event) => {
+        let valor_ant = event.target.innerText
+        if(valor_ant != null){
+            var_op += valor_ant
+            result.setAttribute('value', var_op)
+
+        }
+        
+        console.log(var_op)
+    })
+})
